5fbc38f6afd7e20551acbf47a02162a89d42fb43
Emil Andersson <emilius.andersson10@gmail.com>
2025-02-20 14:23:07 +0100

    Added favicon and a logo to the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="flex justify-evenly">
+      <Link to="/">
+        <img src="src/assets/logo.png" alt="" />
+      </Link>
+      <Link to="/hello">Hello</Link>
+    </header>
+  );
 }
