b642635beabe72a356cba802e84c95268a7d33a8
Emil Andersson <emilius.andersson10@gmail.com>
2025-02-20 14:06:30 +0100

    Added react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,15 @@
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Home from "./pages/Home.jsx";
 
-createRoot(document.getElementById("root")).render(<Home />);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
