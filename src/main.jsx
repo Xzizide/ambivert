6efe97827497abc914a80d133e1e6e3bae455e88
Emil Andersson <emilius.andersson10@gmail.com>
2025-02-20 14:15:39 +0100

    Added a layout with a header and a footer

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,18 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Home from "./pages/Home.jsx";
+import Layout from "./pages/Layout.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
   },
 ]);
 
