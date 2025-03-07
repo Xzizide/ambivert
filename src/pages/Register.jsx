78baf9885fafc8540162c5cae435d696c097f316
Emil Andersson <emilius.andersson10@gmail.com>
2025-03-07 13:18:56 +0100

    Added form components

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,7 +3,13 @@ import React from "react";
 export default function Register() {
   return (
     <main className="bg-amber-50 flex-auto flex">
-      <p>hello</p>
+      <form action="" method="post" className="flex flex-col items-center">
+        <input type="email" name="" id="" className="border" />
+        <input type="password" name="" id="" className="border" />
+        <input type="text" name="" id="" className="border" />
+        <input type="text" name="" id="" className="border" />
+        <input type="submit" value="submit" />
+      </form>
     </main>
   );
 }
