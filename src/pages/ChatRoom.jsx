1e054b37ed3f30849d4a5a268efa9f4789eab11c
Emil Andersson <emilius.andersson10@gmail.com>
2025-03-16 22:49:23 +0100

    Changed the tutorial to use the webcam stream and send it into the websocket as bytes

--- src/pages/ChatRoom.jsx
+++ src/pages/ChatRoom.jsx
@@ -1,13 +1,56 @@
 import React from "react";
-import VideoFrame from "../components/VideoFrame";
+import { useEffect, useRef } from "react";
 
 export default function ChatRoom() {
+  var client_id = Date.now();
+
+  var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
+
+  const localCamRef = useRef();
+
+  const remoteCamRef = useRef();
+
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({
+        video: true,
+      })
+      .then((stream) => {
+        localCamRef.current.srcObject = stream;
+        localCamRef.current.play();
+
+        var mediaRecorder = new MediaRecorder(stream);
+        mediaRecorder.ondataavailable = async function (event) {
+          if (event.data.size > 0 && ws.readyState === WebSocket.OPEN) {
+            ws.send(await event.data.bytes());
+          }
+        };
+
+        mediaRecorder.start();
+
+        setInterval(() => {
+          mediaRecorder.stop();
+          mediaRecorder.start();
+        }, 500);
+      });
+  }, []);
+
+  ws.onmessage = function (event) {
+    var blob = new Blob([event.data], { type: "video/webm; codecs=vp8" });
+    remoteCamRef.current.src = URL.createObjectURL(blob);
+    remoteCamRef.current.play();
+  };
   return (
     <main className="bg-amber-50 flex-auto flex">
       <div className="grid grid-cols-3 gap-3 m-3">
-        <VideoFrame></VideoFrame>
-        <VideoFrame></VideoFrame>
-        <VideoFrame></VideoFrame>
+        <video
+          ref={localCamRef}
+          className="border-2 rounded-2xl bg-amber-500"
+        ></video>
+        <video
+          ref={remoteCamRef}
+          className="border-2 rounded-2xl bg-amber-500"
+        ></video>
       </div>
       <div className="w-200 border rounded-2xl flex flex-col justify-between items-end">
         <ul className="flex flex-col items-end">
