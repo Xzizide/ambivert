855fa074f124cb153c12efbf21106a213cf75150
Emil Andersson <emilius.andersson10@gmail.com>
2025-03-18 09:28:45 +0100

    Changed the structure of the backend and made a camera stream with a websocket

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from fastapi.responses import HTMLResponse
+# from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -7,13 +7,18 @@ app = FastAPI()
 class ConnectionManager:
     def __init__(self):
         self.active_connections: list[WebSocket] = []
+        self.active_ids = []
 
-    async def connect(self, websocket: WebSocket):
+    async def connect(self, websocket: WebSocket, client_id: int):
         await websocket.accept()
         self.active_connections.append(websocket)
+        self.active_ids.append(client_id)
+        for connection in self.active_connections:
+            await connection.send_json(self.active_ids)
 
-    def disconnect(self, websocket: WebSocket):
+    def disconnect(self, websocket: WebSocket, client_id: int):
         self.active_connections.remove(websocket)
+        self.active_ids.remove(client_id)
 
     async def send_personal_message(self, message: str, websocket: WebSocket):
         await websocket.send_text(message)
@@ -28,11 +33,11 @@ manager = ConnectionManager()
 
 @app.websocket("/ws/{client_id}")
 async def websocket_endpoint(websocket: WebSocket, client_id: int):
-    await manager.connect(websocket)
+    await manager.connect(websocket, client_id)
     try:
         while True:
             data = await websocket.receive_bytes()
             await manager.broadcast(data)
     except WebSocketDisconnect:
-        manager.disconnect(websocket)
+        manager.disconnect(websocket, client_id)
         await manager.broadcast(f"Client #{client_id} left the chat")
--- src/pages/ChatRoom.jsx
+++ src/pages/ChatRoom.jsx
@@ -1,24 +1,22 @@
 import React from "react";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function ChatRoom() {
-  var client_id = Date.now();
-
-  var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
-
   const localCamRef = useRef();
-
   const remoteCamRef = useRef();
 
+  const [clients, setClients] = useState([]);
+
   useEffect(() => {
+    var client_id = Date.now();
+
+    var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
+
     navigator.mediaDevices
       .getUserMedia({
         video: true,
       })
       .then((stream) => {
-        localCamRef.current.srcObject = stream;
-        localCamRef.current.play();
-
         var mediaRecorder = new MediaRecorder(stream);
         mediaRecorder.ondataavailable = async function (event) {
           if (event.data.size > 0 && ws.readyState === WebSocket.OPEN) {
@@ -33,24 +31,32 @@ export default function ChatRoom() {
           mediaRecorder.start();
         }, 500);
       });
+
+    ws.onmessage = function (event) {
+      if (typeof event.data === "string") {
+        var client_string = event.data;
+        client_string = client_string.replace("[", "").replace("]", "");
+        setClients(client_string.split(","));
+        console.log(clients);
+      } else {
+        var blob = new Blob([event.data], { type: "video/webm; codecs=vp8" });
+        remoteCamRef.current.src = URL.createObjectURL(blob);
+        remoteCamRef.current.play();
+      }
+    };
   }, []);
 
-  ws.onmessage = function (event) {
-    var blob = new Blob([event.data], { type: "video/webm; codecs=vp8" });
-    remoteCamRef.current.src = URL.createObjectURL(blob);
-    remoteCamRef.current.play();
-  };
   return (
     <main className="bg-amber-50 flex-auto flex">
       <div className="grid grid-cols-3 gap-3 m-3">
-        <video
-          ref={localCamRef}
-          className="border-2 rounded-2xl bg-amber-500"
-        ></video>
-        <video
-          ref={remoteCamRef}
-          className="border-2 rounded-2xl bg-amber-500"
-        ></video>
+        {clients.map(() => {
+          return (
+            <video
+              ref={remoteCamRef}
+              className="border-2 rounded-2xl bg-amber-500"
+            ></video>
+          );
+        })}
       </div>
       <div className="w-200 border rounded-2xl flex flex-col justify-between items-end">
         <ul className="flex flex-col items-end">
